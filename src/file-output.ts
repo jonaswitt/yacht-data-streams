6cbe16671949800588d38d619ce52f8afed1ec2a
Jonas Witt <mail@jonaswitt.com>
2024-04-23 18:00:38 +0200

    jsonl file output format

--- src/file-output.ts
+++ src/file-output.ts
@@ -1,15 +1,54 @@
-import { InfluxDB, WriteApi } from "@influxdata/influxdb-client";
+import { mapKeys, mapValues } from "lodash";
 import { RawPoint, rawPointToInflux } from "./types";
 import * as FileStreamRotator from "file-stream-rotator";
 
 export class FileOutput {
   private logStream: ReturnType<(typeof FileStreamRotator)["getStream"]>;
 
-  constructor(options: Parameters<(typeof FileStreamRotator)["getStream"]>[0]) {
+  private format: "jsonl" | "influx";
+
+  private decimals: number | undefined;
+
+  constructor({
+    format,
+    decimals,
+    ...options
+  }: Parameters<(typeof FileStreamRotator)["getStream"]>[0] & {
+    format?: "jsonl" | "influx";
+    decimals?: number;
+  }) {
     this.logStream = FileStreamRotator.getStream(options);
+    this.format = format ?? "influx";
+    this.decimals = decimals;
   }
 
   write(point: RawPoint) {
-    this.logStream.write(rawPointToInflux(point).toLineProtocol() + "\n");
+    switch (this.format) {
+      case "influx":
+        this.logStream.write(rawPointToInflux(point).toLineProtocol() + "\n");
+        break;
+      case "jsonl":
+        let records = point.fields;
+        if (this.decimals != null) {
+          records = mapValues(records, (value) =>
+            typeof value === "number"
+              ? Math.round(value * 10 ** this.decimals) / 10 ** this.decimals
+              : value
+          );
+        }
+        if (point.tags.instance != null) {
+          records = mapKeys(
+            records,
+            (value, key) => `${key}_${point.tags.instance}`
+          );
+        }
+        this.logStream.write(
+          JSON.stringify({
+            t: point.timestamp.valueOf() / 1000,
+            r: records,
+          }) + "\n"
+        );
+        break;
+    }
   }
 }
